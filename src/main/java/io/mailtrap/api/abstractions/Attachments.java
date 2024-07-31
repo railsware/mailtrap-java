5d26dd652cef1816363604d7582ca6dc6d05469b
vtasun <tasun.v.s@gmail.com>
2024-08-01 01:30:55 +0300

    Added javadoc

--- src/main/java/io/mailtrap/api/abstractions/Attachments.java
+++ src/main/java/io/mailtrap/api/abstractions/Attachments.java
@@ -1,13 +1,35 @@
 package io.mailtrap.api.abstractions;
 
 import io.mailtrap.model.response.AttachmentResponse;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+/**
+ * Interface representing the Mailtrap Testing API for interaction with email message attachments.
+ */
 public interface Attachments {
 
+    /**
+     * Get message single attachment by ID
+     *
+     * @param accountId - unique account ID
+     * @param inboxId - unique inbox ID
+     * @param messageId - unique message ID
+     * @param attachmentId - unique attachment ID
+     * @return attachment details and download path
+     */
     AttachmentResponse getSingleAttachment(long accountId, int inboxId, long messageId, long attachmentId);
 
-    List<AttachmentResponse> getAttachments(long accountId, int inboxId, long messageId, String attachmentType);
+    /**
+     * Get message attachments by inboxId and messageId
+     *
+     * @param accountId - unique account ID
+     * @param inboxId - unique inbox ID
+     * @param messageId - unique message ID
+     * @param attachmentType - attachment type; optional query param
+     * @return attachments with their details and download paths
+     */
+    List<AttachmentResponse> getAttachments(long accountId, int inboxId, long messageId, @Nullable String attachmentType);
 
 }
