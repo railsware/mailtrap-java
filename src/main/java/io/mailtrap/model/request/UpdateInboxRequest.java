d709822cdca9b6e9eb991acd1f75955fcd3edb89
vtasun <tasun.v.s@gmail.com>
2024-08-12 00:52:47 +0300

    Rearranged update and create inbox request models

--- src/main/java/io/mailtrap/model/request/UpdateInboxRequest.java
+++ src/main/java/io/mailtrap/model/request/UpdateInboxRequest.java
@@ -2,6 +2,7 @@ package io.mailtrap.model.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.mailtrap.model.AbstractModel;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,6 +11,19 @@ import lombok.Getter;
 public class UpdateInboxRequest extends AbstractModel {
 
     @JsonProperty("inbox")
-    private UpdateData inboxData;
+    private InboxUpdateData inboxUpdateData;
+
+    @Getter
+    @AllArgsConstructor
+    public static class InboxUpdateData {
+
+        @JsonProperty("name")
+        @Size(min = 2, max = 100)
+        private String name;
+
+        @JsonProperty("email_username")
+        private String emailUsername;
+
+    }
 
 }
