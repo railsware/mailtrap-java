7280679b8d288a9e05ff589d31f23266680fd792
vtasun <tasun.v.s@gmail.com>
2024-10-16 23:20:35 +0300

    Added separate model for sending domain's permissions

--- src/main/java/io/mailtrap/model/response/sending_domains/SendingDomainsResponse.java
+++ src/main/java/io/mailtrap/model/response/sending_domains/SendingDomainsResponse.java
@@ -47,6 +47,6 @@ public class SendingDomainsResponse {
     private String alertRecipientEmail;
 
     @JsonProperty("permissions")
-    private Permission permission;
+    private SendingDomainPermission permission;
 
 }
