6508e05c1e910c680a48634bd4852fdd212ee8f4
Alex Shchyhol <oleksandr.shchyhol@railsware.com>
2026-08-03 14:31:29 +0200

    MT-23076: support expires_at in create api token request

--- src/main/java/io/mailtrap/model/request/apitokens/CreateApiTokenRequest.java
+++ src/main/java/io/mailtrap/model/request/apitokens/CreateApiTokenRequest.java
@@ -1,5 +1,7 @@
 package io.mailtrap.model.request.apitokens;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.mailtrap.model.AbstractModel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,6 +18,19 @@ public class CreateApiTokenRequest extends AbstractModel {
 
     private String name;
 
+    /**
+     * Optional token expiration as an ISO 8601 date-time. Omit (or leave null) for the server
+     * default (a 1-year default is being rolled out). Use {@link TokenExpiration#never()} for
+     * a token that never expires. Past or more-than-5-years-ahead values are rejected with 422.
+     */
+    @JsonProperty("expires_at")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private TokenExpiration expiresAt;
+
     private List<ApiTokenResource> resources;
+
+    public CreateApiTokenRequest(final String name, final List<ApiTokenResource> resources) {
+        this(name, null, resources);
+    }
 
 }
